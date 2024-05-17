0ac1f9932989a49fd9706fca48355658af76259a
Vladimir Litvyakov <flapsya@ya.ru>
2024-05-17 12:24:42 +0800

    Исправил извлечеИсправÐфÑИсправПрисвоил пÐРаспоковал список в отдельные переменные, переделал задание 2

--- homework/vladimir_litvyakov/homework_5/task_2.py
+++ homework/vladimir_litvyakov/homework_5/task_2.py
@@ -3,19 +3,11 @@ result_2 = 'результат операции: 514'
 result_3 = 'результат работы программы: 9'
 
 
-result_1_slice = int(result_1[20:]) + 10
-result_1_index = int(result_1.index('42')) + 10
-print(result_1_slice)
-print(result_1_index)
+number_1 = int(result_1[result_1.index(':')+2:]) + 10
+number_2 = int(result_2[result_2.index(':')+2:]) + 10
+number_3 = int(result_3[result_3.index(':')+2:]) + 10
 
 
-result_2_slice = int(result_2[20:]) + 10
-result_2_index = int(result_2.index('514')) + 10
-print(result_2_slice)
-print(result_2_index)
-
-
-result_3_slice = int(result_3[28:]) + 10
-result_3_index = int(result_3.index('9')) + 10
-print(result_3_slice)
-print(result_3_index)
+print(number_1)
+print(number_2)
+print(number_3)
