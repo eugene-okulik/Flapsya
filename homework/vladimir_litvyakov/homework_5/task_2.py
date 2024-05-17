1c8842b93cb863e009977ec51c426d8376522a83
Vladimir Litvyakov <flapsya@ya.ru>
2024-05-17 12:30:27 +0800

    Добавил пробелыÐ

--- homework/vladimir_litvyakov/homework_5/task_2.py
+++ homework/vladimir_litvyakov/homework_5/task_2.py
@@ -3,9 +3,9 @@ result_2 = 'результат операции: 514'
 result_3 = 'результат работы программы: 9'
 
 
-number_1 = int(result_1[result_1.index(':')+2:]) + 10
-number_2 = int(result_2[result_2.index(':')+2:]) + 10
-number_3 = int(result_3[result_3.index(':')+2:]) + 10
+number_1 = int(result_1[result_1.index(':') + 2:]) + 10
+number_2 = int(result_2[result_2.index(':') + 2:]) + 10
+number_3 = int(result_3[result_3.index(':') + 2:]) + 10
 
 
 print(number_1)
