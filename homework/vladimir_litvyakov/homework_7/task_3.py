d76d565bc3a545c0a2cfaa3b6bb346db62e8dbb6
Vladimir Litvyakov <flapsya@ya.ru>
2024-05-20 22:29:29 +0800

    corrected task_3 add list

--- homework/vladimir_litvyakov/homework_7/task_3.py
+++ homework/vladimir_litvyakov/homework_7/task_3.py
@@ -2,13 +2,12 @@ result_1 = 'результат операции: 42'
 result_2 = 'результат операции: 54'
 result_3 = 'результат работы программы: 209'
 result_4 = 'результат: 2'
+results_list = [result_1, result_2, result_3, result_4]
 
 
 def results(result_num):
     print(int(result_num.split(': ')[-1]) + 10)
 
 
-results(result_1)
-results(result_2)
-results(result_3)
-results(result_4)
+for result in results_list:
+    results(result)
