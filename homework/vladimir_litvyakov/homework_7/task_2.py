f7ab3d19e16b19f3f0ca2cff3af297082216563c
Vladimir Litvyakov <flapsya@ya.ru>
2024-05-21 20:25:10 +0800

    corrected аdef on for

--- homework/vladimir_litvyakov/homework_7/task_2.py
+++ homework/vladimir_litvyakov/homework_7/task_2.py
@@ -1,12 +1,6 @@
 words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
 
 
-def words_multiply(key, value):
-    result = list(words)[key] * words[value]
-    print(result)
-
-
-words_multiply(0, 'I')
-words_multiply(1, 'love')
-words_multiply(2, 'Python')
-words_multiply(3, '!')
+for key in words:
+    value = words[key]
+    print(key * value)
